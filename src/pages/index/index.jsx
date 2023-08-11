c9d60022a0e98721b438174820468ab952e16fba
c13670672980 <420509089@qq.com>
2023-08-11 09:04:25 +0800

    cui

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -50,6 +50,7 @@ export default function Index() {
   return (
     <div>
       <Text>滚动加载</Text>
+      
       <ul id="scroller" className={style.list}>
         <LoadMore 
           target='scroller' 
